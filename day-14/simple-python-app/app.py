a60cb720a61cc9c75ba7927dddc8741fb292db44
bsk111675 <bsk111675@gmail.com>
2025-07-10 14:54:29 +0530

    This is my first CI/CD project

--- day-14/simple-python-app/app.py
+++ day-14/simple-python-app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, world! I have commited new commit'
+    return 'Hello, world! This is my First CI/CD pipeline Project'
 
 if __name__ == '__main__':
     app.run()
